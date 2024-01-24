f7716799431f0a2db8124dc6e15f2a42d330f8e6
Unleashdraptr <98326571+Unleashdraptr@users.noreply.github.com>
2024-01-24 12:17:20 +0000

    kai changes

--- Assets/Scripts/Player/MovePlayer.cs
+++ Assets/Scripts/Player/MovePlayer.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class movePlayer : MonoBehaviour
+public class MovePlayer : MonoBehaviour
 {
 
     [SerializeField]
     private Animator anim;
 
     Rigidbody rb;
-    public float thrust;
+    public float thrust = 3200f;
 
     public float sensitivity = 1000f;
 
