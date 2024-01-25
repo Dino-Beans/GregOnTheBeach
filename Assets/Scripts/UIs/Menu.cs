3ac253eaa69d7cdb11f31dafbba7621882ad6fb7
Kai <kai.hafner@outlook.com>
2024-01-25 12:08:33 +0000

    MenuStuff

---
+++ Assets/Scripts/UIs/Menu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void playBtnPressed()
+    {
+        SceneManager.LoadScene("BeachLevelBlockout");
+    }
+    public void ControlssbtnPressed()
+    {
+        SceneManager.LoadScene("ControlScreen");
+    }
+
+    public void QuitbtnPressed()
+    {
+        Application.Quit();
+    }
+
+
+}
