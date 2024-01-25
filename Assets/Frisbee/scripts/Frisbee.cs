007eb8c38cc3835802b23b5fcb2cd2b3a15ca91d
Owen <wickenowen@gmail.com>
2024-01-25 13:38:57 +0000

    Beach Level more SHIT

---
+++ Assets/Frisbee/scripts/Frisbee.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Frisbee : MonoBehaviour
+{
+
+    private Rigidbody frisbody;
+    private Transform catcher;
+    private Transform thrower;
+    private bool held = true;
+
+    [Header("Throw Parameters")]
+    public float impulse = 30;
+    public float heightOffset = 0.2f;
+    public float widthOffsetRange = 10f;
+    public float frisbeeMass = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thrower = transform.parent;
+        Transform gameParent = transform.parent.parent;
+        for (int i = gameParent.childCount - 1; i >= 0; i--)
+        {
+            Transform t = gameParent.GetChild(i);
+            if (t != thrower)
+            {
+                catcher = t;
+                break;
+            }
+        }
+        frisbody = GetComponent<Rigidbody>();
+        frisbody.mass = frisbeeMass;
+        throwFrisbee();
+    }
+
+    private float calculateFlightRange(Vector3 Vx, Vector3 V0)
+    {
+        float alpha = Vector3.Angle(Vx, V0) * Mathf.Deg2Rad;
+        return Mathf.Pow(V0.magnitude, 2) * Mathf.Sin(2 * alpha) / 9.8f;
+    }
+
+    private Vector3 splitHypotenuse(float h)
+    {
+        //split horizontal offset based on rotation
+        float angle = transform.rotation.eulerAngles.y * Mathf.Deg2Rad;
+        return new Vector3(h * Mathf.Sin(angle), 0, h * Mathf.Cos(angle));
+    }
+
+    private void throwFrisbee()
+    {
+        // b - a
+        Vector3 a = thrower.position;
+        Vector3 b = catcher.position;
+
+        float widthOffset = Random.Range(-widthOffsetRange, widthOffsetRange);
+
+        Vector3 horizontalOffset = splitHypotenuse(widthOffset);
+
+        Vector3 Vx = ((b - a).normalized * impulse) + horizontalOffset;
+        Vector3 Vy = new Vector3(0, heightOffset, 0) * impulse;
+
+        // calculate initial velocity of the projectile
+        Vector3 V0 = Vx + Vy;
+        Debug.Log("initial velocity: " + V0);
+
+        held = false;
+        frisbody.AddForce(V0, ForceMode.Impulse);
+        frisbody.useGravity = true;
+
+        float throwRange = calculateFlightRange(Vx, V0);
+
+        catcher.GetComponent<catcher>().chaseFrisbee(throwRange, thrower, Vx);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+
+        if (other.transform == catcher)
+        {
+            // stop frisbee
+            frisbody.useGravity = false;
+            frisbody.angularVelocity = Vector3.zero;
+            frisbody.velocity = Vector3.zero;
+            held = true;
+            catcher.GetComponent<catcher>().catchFrisee();
+
+            transform.SetParent(catcher);
+            // swapping catcher and thrower
+            Transform temp = thrower;
+            thrower = catcher;
+            catcher = temp;
+
+            StartCoroutine(waitBeforeThrow());
+        }
+    }
+
+    private void Update()
+    {
+        if (held)
+        {
+            Vector3 goTo = thrower.GetComponent<catcher>().getFrisbeePosition();
+            transform.position = goTo;
+        }
+    }
+
+    IEnumerator waitBeforeThrow()
+    {
+        yield return new WaitForSeconds(2.5f);
+        throwFrisbee();
+    }
+}
---
+++ Assets/Frisbee/scripts/catcher.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class catcher : MonoBehaviour
+{
+    private bool hasTarget = false;
+    private Vector3 catchTarget;
+    public Transform frisbeePosition;
+    public Animator animator;
+    public float speed;
+
+    public void chaseFrisbee(float throwRange, Transform thrower, Vector3 Vx)
+    {
+        Debug.Log("magnitude: " + throwRange);
+        Vector3 R = Vx.normalized * throwRange;
+
+        Debug.Log("x velocity: " + Vx);
+
+        catchTarget = thrower.position + R;
+        hasTarget = true;
+    }
+
+    public Vector3 getFrisbeePosition()
+    {
+        return frisbeePosition.position;
+    }
+
+    public void catchFrisee()
+    {
+        animator.SetTrigger("catch");
+        
+    }
+
+    private void Update()
+    {
+        if (hasTarget)
+        {
+            float time = Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, catchTarget, speed * time);
+            if (catchTarget.x - transform.position.x < 0)
+            {
+                animator.SetBool("running right", true);
+                animator.SetBool("running left", false);
+            } 
+            else if (catchTarget.x - transform.position.x > 0)
+            {
+                animator.SetBool("running right", false);
+                animator.SetBool("running left", true);
+            }
+            else
+            {
+                animator.SetBool("running right", false);
+                animator.SetBool("running left", false);
+            }
+        }
+    }
+}
